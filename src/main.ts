1407f4e20ab4f30b41175bbc7e1a05228a94e8d1
Des1derium <43518696+AinfinitedZ@users.noreply.github.com>
2023-12-05 11:30:47 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
+import { ModCallback } from "isaac-typescript-definitions";
+import { ISCFeature, upgradeMod } from "isaacscript-common";
 import { activeOnUse } from "./reprex";
+
 const MOD_NAME = "reprex";
-export const mod = upgradeMod(modVanilla);
 const modVanilla = RegisterMod(MOD_NAME, 1);
+const features = [ISCFeature.RUN_NEXT_ROOM] as const;
+export const mod = upgradeMod(modVanilla, features);
 
 export function main(): void {
   mod.AddCallback(
     ModCallback.PRE_USE_ITEM,
     activeOnUse,
-    Isaac.GetItemIdByName("The D6")
+    Isaac.GetItemIdByName("The D6"),
   );
 }
